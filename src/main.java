7c54e9b16ab0eb61b92b72e82fdb06ea8cf9cda1
Student <student@itpark.ru>
2019-09-14 15:04:42 +0300

    Logical without limit

--- src/main.java
+++ src/main.java
@@ -2,7 +2,20 @@ import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 public class main {
     public static void main(String[] args) {
+       int amountInRegular = 100;
+       int amountInHigh = 1000;
+       int amountInSpecial = 10_000;
 
+       int percentInRegular = 1;
+       int percentInHigh = 5;
+       int percentInCpecial = 30;
+
+       int result = (
+               amountInRegular * percentInRegular +
+               amountInHigh * percentInHigh +
+               amountInSpecial * percentInCpecial
+       ) / 100;
+       System.out.println(result);
     }
 }
 
